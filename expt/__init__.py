c73b7ac6ffd269e5f5cd42881fc71b4ec08963a7
Jongwook Choi <wookayin@gmail.com>
2022-02-07 13:43:04 -0500

    Release version: 0.3.0

--- expt/__init__.py
+++ expt/__init__.py
@@ -3,7 +3,7 @@ import sys
 if sys.version_info < (3, 6):
   raise RuntimeError("This library requires python 3.6+.")
 
-__version__ = '0.3.0.dev0'
+__version__ = '0.3.0'
 
 # auto-import submodules
 from . import colors as colors
