117206d1b5eb60dd7896db6234308f068579c1e6
Jongwook Choi <wookayin@gmail.com>
2022-02-13 17:10:07 -0500

    Separate data loading functions to data_loader module.

--- expt/__init__.py
+++ expt/__init__.py
@@ -15,5 +15,5 @@ from .data import Experiment as Experiment
 from .data import Hypothesis as Hypothesis
 from .data import Run as Run
 from .data import RunList as RunList
-from .data import get_runs as get_runs
-from .data import parse_run as parse_run
+from .data_loader import get_runs as get_runs
+from .data_loader import parse_run as parse_run
