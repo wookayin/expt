dc6df06e7267c0161e8c1c4ad2c6665db031d61d
Jongwook Choi <wookayin@gmail.com>
2022-10-01 20:32:47 -0400

    Release version: 0.4.1
    Minor bugfixes to 0.4.0 release.

--- expt/__init__.py
+++ expt/__init__.py
@@ -3,7 +3,7 @@ import sys
 if sys.version_info < (3, 6):
   raise RuntimeError("This library requires python 3.6+.")
 
-__version__ = '0.5.0.dev0'
+__version__ = '0.4.1'
 
 # auto-import submodules
 from . import colors as colors
